baf105702652b18cd29a9122c859667ce29a074b
jrohkohl <jrohkohl@chicagobooth.edu>
2021-04-02 02:31:53 +0000

    Completed koan

---
+++ test.rb
@@ -0,0 +1,12 @@
+empty_array = Array.new
+#assert_equal Array, empty_array.class
+#assert_equal nil, empty_array.size
+
+
+print(empty_array.size)
+
+    #assert_equal __, 1.is_a?(Object)
+    #assert_equal __, 1.5.is_a?(Object)
+    #assert_equal __, "string".is_a?(Object)
+    #assert_equal __, nil.is_a?(Object)
+    #assert_equal __, Object.is_a?(Object)
