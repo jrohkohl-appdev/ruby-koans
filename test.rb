fc458d08c8bcdddf151e624f3101640bde0a2387
jrohkohl <jrohkohl@chicagobooth.edu>
2021-04-02 18:09:35 +0000

    Completed koan

--- test.rb
+++ test.rb
@@ -1,9 +1,18 @@
-empty_array = Array.new
+first_name, last_name = ["John", "Smith"]
 #assert_equal Array, empty_array.class
 #assert_equal nil, empty_array.size
 
+#if :peanut == array[0,1]
+  #print(true)
 
-print(empty_array.size)
+
+#else
+  #print(false)
+
+  
+#end
+
+print(last_name)
 
     #assert_equal __, 1.is_a?(Object)
     #assert_equal __, 1.5.is_a?(Object)
